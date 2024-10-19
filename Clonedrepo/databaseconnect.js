518c6a1386ea681f56566f19a584356dca8aa996
ieeedevops <110836015+nio2004@users.noreply.github.com>
2024-10-19 13:39:11 +0530

    test files

--- Clonedrepo/databaseconnect.js
+++ Clonedrepo/databaseconnect.js
@@ -40,6 +40,7 @@ async function run() {
         // Close the connection
         await client.close();
         console.log('Connection closed.');
+        console.log('Connection closed.');
     }
 }
 
